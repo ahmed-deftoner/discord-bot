aadc81776ae125edf92458dd0a09052a220b52a3
ahmed-deftoner <70218377+ahmed-deftoner@users.noreply.github.com>
2022-08-12 19:34:20 +0500

    config struct

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,1 +1,12 @@
 package config
+
+type configStruct struct {
+	Token     string `json:"Token"`
+	BotPrefix string `json:"BotPrefix"`
+}
+
+var (
+	Token     string
+	BotPrefix string
+	Config    *configStruct
+)
