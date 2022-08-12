180d3544823eb9873ee78aafa83bba4bb242ac99
ahmed-deftoner <70218377+ahmed-deftoner@users.noreply.github.com>
2022-08-12 19:26:45 +0500

    main.go

--- main.go
+++ main.go
@@ -1,1 +1,16 @@
 package main
+
+import "fmt"
+
+func main() {
+	err := config.ReadConfig()
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	bot.Start()
+
+	<-make(chan struct{})
+	return
+}
