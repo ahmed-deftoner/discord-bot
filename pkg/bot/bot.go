aaaa8e2e49f8451eccfa98ed83a9158b0afc2d30
ahmed-deftoner <70218377+ahmed-deftoner@users.noreply.github.com>
2022-08-12 19:51:41 +0500

    bot start

--- pkg/bot/bot.go
+++ pkg/bot/bot.go
@@ -1,1 +1,31 @@
 package bot
+
+import (
+	"fmt"
+
+	"github.com/ahmed-deftoner/discord-bot/pkg/config"
+	"github.com/bwmarrin/discordgo"
+)
+
+var BotID string
+var goBot *discordgo.Session
+
+func Start() {
+	goBot, err := discordgo.New("Bot" + config.Token)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	u, err := goBot.User("@me")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	BotID = u.ID
+	goBot.AddHandler(messageHandler)
+	err = goBot.Open()
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	fmt.Println("horny bot is running...")
+}
